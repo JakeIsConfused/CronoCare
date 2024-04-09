5b1860c78a0359d671b82de4f54ba51154da7628
JakeisConfused <jjmacandrew7@gmail.com>
2024-04-09 11:01:18 +0200

    Css en Html

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         <img  class="logo" src="css/img/logo.png">
     </div>
     <div class="setting">
-        <a href="php/settings.php" class="indexA"><img class="settingKnop" src="css/img/settings.png" alt="afbeelding van instelling knop"></a>
+        <button><a href="php/settings.php"><img class="settingKnop" src="css/img/settings.png" alt="afbeelding van instelling knop"></a></button>
 
     </div>
 </div>
